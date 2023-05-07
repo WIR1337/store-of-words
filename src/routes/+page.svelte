12f7b5c05ba22911849ac66c4617cc4a9d398060
Alexandr N <naberezhnyy.an@gmail.com>
2023-05-07 11:27:21 +0200

    start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-    import main from '../scripts/main.js'
+    let newSentence = ''
+    
+    let sentenceArr:string[] = []
+
+    function saveSentence(){
+        sentenceArr.push(newSentence)
+        sentenceArr = sentenceArr
+    }
+
 </script>
 
-<div>Wellcome back</div>
+{#each sentenceArr as sentence}
+    <div>{sentence}</div>
+{/each}
+<textarea bind:value={newSentence} class="border-2 border-blue-500" id="sentenceInput" cols="30" rows="5"></textarea>
+<button on:click={saveSentence} class="border border-black">Add</button>
